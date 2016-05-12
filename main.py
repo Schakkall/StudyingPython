a54f2ce667a69a85dd878dc4b376a206adae9b3a
RaZ <jeuriqueribeiro@gmail.com>
2016-05-12 10:04:58 -0300

    Preparando código para apresentação

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date
 
 
 def clear():
@@ -8,9 +9,12 @@ def clear():
 class Person:
     __code = 0
     __name = None
-    __birth = 0
+    __birth = None
     __email = None
     __phone = None
+
+    def __init__(self, code):
+        self.__code = code
 
     def __init__(self, code, name, birth, email, phone):
         self.__code = code
@@ -20,8 +24,24 @@ class Person:
         self.__phone = phone
 
     def presents(self):
-        print("Hello , my name is %s , I was born in %s , my email is %s and my phone number is %s"% \
-              (self.__name, self.__birth, self.__email, self.__phone))
+        print("Hello , my name is {name:s} , I was born in {birth:s} , my email is {mail:s} and my phone number is {pho\
+ne:s}".format(name=self.__name, birth=self.__birth, mail=self.__email, phone=self.__phone))
+
+    def tell_your_age(self):
+        t = date.today()
+        print("I'm {0:s}".format(str(t.year - int(self.__birth[6:]))))
+
+    def test_variable_scope(self):
+        a = 0
+
+        def test2():
+            b = 0
+            a = 1
+            print(a)
+
+        test2()
+        print(a)
+
 
 clear()
 
@@ -30,6 +50,13 @@ print("\nHello World! Welcome to my Git testing!")
 p = Person(0, "Jose", "15/06/1993", "somemail@gmail.com", "99900-0000")
 
 p.presents()
+
 Person.presents(p)
+Person.tell_your_age(p)
+# Person.test_variable_scope(p)
+# Person.clear(p)
+Person.presents(p)
+# print(p.)
 
-
+# print(p._Person__name)
+# print(p.__name)
